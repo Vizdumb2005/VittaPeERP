d2dd4110c7d9ac210a5692bed6e10ef8ea428fa9
sudarsan2001 <frankel9675@gmail.com>
2026-04-02 18:45:16 +0530

    fix: display sales invoice fields in POS additional fields when invoice type is Sales Invoice

--- erpnext/accounts/doctype/pos_settings/pos_settings.js
+++ erpnext/accounts/doctype/pos_settings/pos_settings.js
@@ -41,10 +41,13 @@ frappe.ui.form.on("POS Settings", {
 		frm.trigger("get_invoice_fields");
 		frm.trigger("add_search_options");
 	},
-
+	invoice_type: function (frm, doctype, name) {
+		frm.trigger("get_invoice_fields");
+	},
 	get_invoice_fields: function (frm) {
-		frappe.model.with_doctype("POS Invoice", () => {
-			var fields = $.map(frappe.get_doc("DocType", "POS Invoice").fields, function (d) {
+		var invoice_type = frm.doc.invoice_type ? frm.doc.invoice_type : "POS Invoice";
+		frappe.model.with_doctype(invoice_type, () => {
+			var fields = $.map(frappe.get_doc("DocType", invoice_type).fields, function (d) {
 				if (
 					frappe.model.no_value_type.indexOf(d.fieldtype) === -1 ||
 					["Button"].includes(d.fieldtype)
@@ -101,9 +104,12 @@ frappe.ui.form.on("POS Search Fields", {
 frappe.ui.form.on("POS Field", {
 	fieldname: function (frm, doctype, name) {
 		var doc = frappe.get_doc(doctype, name);
-		var df = $.map(frappe.get_doc("DocType", "POS Invoice").fields, function (d) {
-			return doc.fieldname == d.fieldname ? d : null;
-		})[0];
+		var df = $.map(
+			frappe.get_doc("DocType", frm.doc.invoice_type ? frm.doc.invoice_type : "POS Invoice").fields,
+			function (d) {
+				return doc.fieldname == d.fieldname ? d : null;
+			}
+		)[0];
 
 		doc.label = df.label;
 		doc.reqd = df.reqd;

ad643a79501c6b025259d187e27b01e7b622acc8
ervishnucs <ervishnucs369@gmail.com>
2026-04-05 22:21:41 +0530

    test: resolve failed testcases for exchage rate

--- erpnext/accounts/doctype/payment_entry/test_payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/test_payment_entry.py
@@ -508,6 +508,8 @@ class TestPaymentEntry(ERPNextTestSuite):
 		si.submit()
 
 		pe = get_payment_entry("Sales Invoice", si.name, bank_account="_Test Bank - _TC", bank_amount=4700)
+		pe.source_exchange_rate = 50
+		pe.set_amounts()
 		pe.reference_no = si.name
 		pe.reference_date = nowdate()
 
@@ -583,6 +585,8 @@ class TestPaymentEntry(ERPNextTestSuite):
 		pe = get_payment_entry(
 			"Sales Invoice", si.name, party_amount=20, bank_account="_Test Bank - _TC", bank_amount=900
 		)
+		pe.source_exchange_rate = 50
+		pe.set_amounts()
 		pe.reference_no = "1"
 		pe.reference_date = "2016-01-01"
 
